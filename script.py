646ccda3f09e432172265d7a2a6803a0c17a0a59
rihaal-0 <84786216+rihaal-0@users.noreply.github.com>
2021-06-15 19:33:58 +0530

    Create script.py

--- script.py
+++ script.py
@@ -53,17 +53,17 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Seriesland4U_Netflix_HBO</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot </b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Rihu_mone
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>English chatting group :</b> <a href='https://t.me/English_chatting_club_Global'>Video Link</a> 
 
 """
